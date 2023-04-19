80b13798d72d17d3d17fd66e288a8f0a5340eb11
Kunal Dubey <xakep8@protonmail.com>
2023-04-19 20:14:58 +0530

    Added changes for adding a todo in gui

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,39 +2,47 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
-const path=require("path");
+const path = require("path");
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-const formattedDate = d => {
-  return d.toISOString().split("T")[0]
-}
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
+};
 
-var dateToday = new Date()
-const today = formattedDate(dateToday)
+var dateToday = new Date();
+const today = formattedDate(dateToday);
 const yesterday = formattedDate(
   new Date(new Date().setDate(dateToday.getDate() - 1))
-)
+);
 const tomorrow = formattedDate(
   new Date(new Date().setDate(dateToday.getDate() + 1))
-)
+);
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/",async function(request,response){
+app.get("/", async function (request, response) {
   Todo.deleteAll();
-  Todo.addTodo({ title: 'Submit assignment', dueDate: yesterday, completed: false });
-  Todo.addTodo({ title: 'Pay rent', dueDate: today, completed: true });
-  Todo.addTodo({ title: 'Service Vehicle', dueDate: today, completed: false });
-  Todo.addTodo({ title: 'File taxes', dueDate: tomorrow, completed: false });
-  Todo.addTodo({ title: 'Pay electric bill', dueDate: tomorrow, completed: false });
-  const allTodos=await Todo.getTodos();
-  if(request.accepts("html")){
-    response.render("index",{allTodos});
-  }
-  else{
-    response.json({allTodos});
+  Todo.addTodo({
+    title: "Submit assignment",
+    dueDate: yesterday,
+    completed: false,
+  });
+  Todo.addTodo({ title: "Pay rent", dueDate: today, completed: true });
+  Todo.addTodo({ title: "Service Vehicle", dueDate: today, completed: false });
+  Todo.addTodo({ title: "File taxes", dueDate: tomorrow, completed: false });
+  Todo.addTodo({
+    title: "Pay electric bill",
+    dueDate: tomorrow,
+    completed: false,
+  });
+  const allTodos = await Todo.getTodos();
+  if (request.accepts("html")) {
+    response.render("index", { allTodos });
+  } else {
+    response.json({ allTodos });
   }
 });
 
@@ -60,9 +68,13 @@ app.get("/todos/:id", async function (request, response) {
 });
 
 app.post("/todos", async function (request, response) {
+  console.log("Creating a todo", request.body);
   try {
-    const todo = await Todo.addTodo(request.body);
-    return response.json(todo);
+    await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+    });
+    return response.redirect("/");
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
@@ -89,19 +101,21 @@ app.delete("/todos/:id", async function (request, response) {
   // response.send(true)
   Todo.destroy({
     where: {
-       id: request.params.id //this will be your id that you want to delete
-    }
-  }).then(function(rowDeleted){
-    console.log(rowDeleted); // rowDeleted will return number of rows deleted
-   if(rowDeleted === 1){
-      response.send(true);
-    }
-    else{
+      id: request.params.id, //this will be your id that you want to delete
+    },
+  }).then(
+    function (rowDeleted) {
+      console.log(rowDeleted); // rowDeleted will return number of rows deleted
+      if (rowDeleted === 1) {
+        response.send(true);
+      } else {
+        response.send(false);
+      }
+    },
+    function () {
       response.send(false);
     }
-  }, function(err){
-    response.send(false);
-  });
+  );
 });
 
 module.exports = app;
